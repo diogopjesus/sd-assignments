41fb89a626c090f1d2f3853582e7cb22160fa17f
Diogo Jesus <diogopjesus@ua.pt>
2023-05-06 05:46:56 +0100

    Update package-infos across project.

--- src/commInfra/package-info.java
+++ src/commInfra/package-info.java
@@ -1,4 +1,8 @@
 /**
- * Common infrastructure.
+ * Common infrastructure for the Heist to the Museum.
+ *
+ * Implementation of a client-server model of type 2 (server replication). Communication is based on
+ * a communication channel under the TCP protocol.
  */
+
 package commInfra;
--- src/serverSide/package-info.java
+++ src/serverSide/package-info.java
@@ -1,1 +1,7 @@
+/**
+ * Server side of the Heist to the Museum.
+ *
+ * Implementation of a client-server model of type 2 (server replication). Communication is based on
+ * a communication channel under the TCP protocol.
+ */
 package serverSide;
