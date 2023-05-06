b68bf8f169f8a0b5ce1d101f7c547cd6258ac16f
Diogo Jesus <diogopjesus@ua.pt>
2023-05-06 01:39:17 +0100

    Added clientSide package info documentation.

--- src/clientSide/package-info.java
+++ src/clientSide/package-info.java
@@ -1,1 +1,8 @@
+/**
+ * Client side of the Heist to the Museum.
+ *
+ * Implementation of a client-server model of type 2 (server replication). Communication is based on
+ * a communication channel under the TCP protocol.
+ */
+
 package clientSide;
